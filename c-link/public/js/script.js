350735094e182fcbdf68dcbddf894a920640e0e2
hugo <helc85.info@gmail.com>
2022-10-25 21:18:42 +0200

    C-Link continuation...

--- c-link/public/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $("#txtDate").datepicker({
-        showOn: 'button',
-        buttonImageOnly: true,
-        buttonImage: 'images/calendar.gif',
-        dateFormat: 'dd/mm/yy'
-    });
-});
