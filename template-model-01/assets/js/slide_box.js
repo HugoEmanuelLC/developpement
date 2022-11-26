61f12633ec35bcfa5af4b7c2525c27f8e4132a05
hugo clavinas <helc85.info@gmail.com>
2022-11-26 02:29:34 +0100

    model mon portfolio v01...

--- template-model-01/assets/js/slide_box.js
+++ template-model-01/assets/js/slide_box.js
@@ -1,16 +1,17 @@
 $(function(){
     
-    let a_apropos = $(".a-apropos");
-    let apropos = $(".apropos");
-    let apropos_span = $(".apropos span");
+    let a_in_slide_box = $(".a_in_slide_box");
+    let a_out_slide_box = $(".a_out_slide_box");
+    
+    let slide_box = $(".slide_box");
 
 
     
-
-    a_apropos.click(function(){
-        apropos.addClass("show").removeClass("hide")
+    
+    a_in_slide_box.click(function(){
+        slide_box.addClass("show").removeClass("hide")
     });
-    apropos_span.click(function(){
-        apropos.addClass("hide").removeClass("show")
+    a_out_slide_box.click(function(){
+        slide_box.addClass("hide").removeClass("show")
     });
 })
